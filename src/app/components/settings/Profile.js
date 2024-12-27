8e643eddc0c3f130facd5134b8875e30783b0bd3
Girish_Gaikwad <145460775+girish-gaikwad@users.noreply.github.com>
2024-12-27 15:51:12 +0530

    Update Profile.js

--- src/app/components/settings/Profile.js
+++ src/app/components/settings/Profile.js
@@ -6,7 +6,7 @@ const Profile = () => {
 		<SettingSection icon={User} title={"Profile"}>
 			<div className='flex flex-col sm:flex-row items-center mb-6'>
 				<img
-					src='./Profile.jpg'
+					src='./profile.jpg'
 					alt='Profile'
 					className='rounded-full w-20 h-20 object-cover mr-4'
 				/>
